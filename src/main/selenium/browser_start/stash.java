23ffab7a6bde8380ec3166ba2fd1f7df00103ef5
moreeds4@gmail.com <Moreed@Muniras-MacBook-Air.local>
2024-04-28 22:57:08 -0700

    "mergin cart page"

--- src/main/selenium/browser_start/stash.java
+++ src/main/selenium/browser_start/stash.java
@@ -2,6 +2,10 @@ package browser_start;
 
 public class stash {
 	public static void main(String[] args) {
-		System.out.println("for stashing2");
+
+		System.out.println("for stashing1");
+		if(true){
+			System.out.println("add more feature");
+		}
 	}
 }
